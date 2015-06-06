a13b902ba2e5589b5e3d338c373d1ef578cf7b2b
Katie Schilling <kschilling@enova.com>
2015-06-06 02:21:37 -0500

    sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-  root 'positions#index#'
+  root               'positions#index#'
+  
+  get     'login' => 'sessions#new'
+  post    'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :positions
   # Example of regular route:
+
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
